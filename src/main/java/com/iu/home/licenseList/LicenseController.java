ad782619c88310b8526699b56bc5573027c81ede
junsolhee7 <junsolhee7@naver.com>
2022-09-30 09:41:50 +0900

    1

--- src/main/java/com/iu/home/licenseList/LicenseController.java
+++ src/main/java/com/iu/home/licenseList/LicenseController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
