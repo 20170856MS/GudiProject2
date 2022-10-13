14603c47b27f218b9e73458cfc0d582d5fdf8ab5
EUNSH1 <tkdgus968@naver.com>
2022-10-14 03:42:21 +0900

    errors까지 끝

--- src/main/java/com/iu/home/error/FrontErrorController.java
+++ src/main/java/com/iu/home/error/FrontErrorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value ="/error/*")
+@RequestMapping(value ="/errors/*")
 public class FrontErrorController {
 
 	@GetMapping("error400")
@@ -15,7 +15,6 @@ public class FrontErrorController {
 		
 		System.out.println("Parameter가 틀림");
 		mv.setViewName("errors/error_404");
-		
 		
 		return mv;
 	}
